e604ef94c5c05d6243cd4fa0898af0e329b1b1c0
Tallispt <tallispt@gmail.com>
2024-05-07 18:19:15 -0300

    feat: validation from file and form added

--- ws_api/middlewares/validation.py
+++ ws_api/middlewares/validation.py
@@ -18,6 +18,48 @@ def validate_body(func, schema):
         
     return decorated
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def validate_file(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        try:
+            data = request.files
+            if 'file' not in data:
+                raise ValidationError("No 'file' found.")
+            
+            file = data['file']
+            if file.filename == '':
+                raise ValidationError('No selected file.')
+            
+            if not file or not allowed_file(file.filename):
+                raise ValidationError('File not supported.')
+            return func(*args, **kwargs)
+        except ValidationError as e:
+            return {'error': str(e.messages)}, HTTPStatus.FORBIDDEN
+        
+    return decorated
+
+def validate_form(func, schema):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        try:
+            form = request.form
+            result = schema().load(form)
+            for item in result:
+                if(result[item] == ""):
+                    raise ValidationError("Not allowed blanck value ['" + item + "']")
+
+            return func(*args, **kwargs)
+        except ValidationError as e:
+            return {'error': str(e.messages)}, HTTPStatus.FORBIDDEN
+        
+    return decorated
+
 # def validate_query(func, query, schema):
 #     @wraps(func)
 #     def decorated(*args, **kwargs):
