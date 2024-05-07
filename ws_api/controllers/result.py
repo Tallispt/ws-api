2d7de5b4464f86c01400e988fbb342b109ebf05c
Tallispt <tallispt@gmail.com>
2024-05-07 18:20:49 -0300

    feat: result router changes

--- ws_api/controllers/result.py
+++ ws_api/controllers/result.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
 from http import HTTPStatus
+from functools import partial
 
 from ..services import result
 from ..middlewares.authorization import token_required
+from ..middlewares.validation import validate_body
+from ..schemas.data import DataBodySchema
+
+validate = partial(validate_body, schema = DataBodySchema)
 
 result_bp = Blueprint("result", __name__, url_prefix="/result")
 
@@ -10,8 +15,43 @@ result_bp = Blueprint("result", __name__, url_prefix="/result")
 @token_required
 def get_results():
   try:
-    response = result.find()
+    response = result.find_all_results()
     return response, HTTPStatus.OK
     
   except Exception as e:
       return {'error': str(e)}, HTTPStatus.BAD_REQUEST
+  
+@result_bp.route('/<id>', methods=['GET'])
+@token_required
+def find_result(id):
+  try:
+    response = result.find_result(id)
+    return response, HTTPStatus.OK
+    
+  except Exception as e:
+    if(str(e) == 'Not_found_error'):
+      return {'error': "Result not found!"}, HTTPStatus.NOT_FOUND
+    return {'error': str(e)}, HTTPStatus.BAD_REQUEST
+  
+@result_bp.route('/<id>', methods=['PUT'])
+@token_required
+@validate
+def put_result(id):
+  try:
+      response = result.update_result(id)
+      return response, HTTPStatus.OK
+  
+  except Exception as e:
+      return {'error': str(e)}, HTTPStatus.BAD_REQUEST
+    
+@result_bp.route('/<id>', methods=['DELETE'])
+@token_required
+def delete_result(id):
+  try:
+    response = result.remove_result(id)
+    return response, HTTPStatus.OK
+  
+  except Exception as e:
+    if(str(e) == "'NoneType' object is not subscriptable"):
+        return {'error': "Result not found!"}, HTTPStatus.NOT_FOUND
+    return {'error': str(e)}, HTTPStatus.BAD_REQUEST
--- ws_api/services/result.py
+++ ws_api/services/result.py
@@ -1,15 +1,58 @@
 from flask import request
 from bson import json_util
 
-from ..repositories import result
+from ..services import data as data_service
+from ..repositories import result, data
 from ..utils import cammel_snake
 
-def find():
-  result_data = result.find_all()
+def find_all_results():
+  user_id = request.environ['user_id']
+
+  result_data = result.find_all(user_id)
 
   if(not result_data):
     return []
 
   list_cur = list(result_data)
   response = json_util.dumps(list_cur)
+  #TODO return stream
   return response
+
+def find_result(id):
+  user_id = request.environ['user_id']
+
+  result_data = result.find_by_id(user_id, id)
+
+  if(not result_data):
+    raise Exception("Not_found_error")
+
+  response = json_util.dumps(result_data)
+
+    #TODO return stream
+  return response
+
+def update_result(id):
+  user_id = request.environ['user_id']
+
+  body = request.json
+  _body = cammel_snake.convert_json(body)
+
+  result_exists = result.find_by_id(user_id, _body["_id"])
+
+  if(not result_exists):
+    return Exception("Not_found_error")
+  
+  if((result_exists["mode_id"] != _body["mode_id"]) or (result_exists["files"] != _body["files"])):
+    #TODO Se mode_id e files não forem iguais, refazer result + data (POST)
+    return data_service.update_data()
+
+  updated_result = result.update(user_id, id, _body["name"])
+  return {"resultId": str(updated_result["_id"])}
+
+def remove_result(id):
+  user_id = request.environ['user_id']
+
+  deleted_result = result.delete(user_id, id)
+  deleted_data = data.delete(user_id, deleted_result["data_id"])
+
+  return {"resultId": str(deleted_result["_id"]), "dataId": str(deleted_data["_id"])}
